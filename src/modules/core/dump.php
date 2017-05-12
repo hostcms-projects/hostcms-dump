f1f487e3a91d81544db05cf125b567c0af7a49a2
maximzasorin <maximzasorin@gmail.com>
2017-05-12 17:53:00 +0300

    basic dump realization

--- src/modules/core/dump.php
+++ src/modules/core/dump.php
@@ -8,6 +8,31 @@ defined('HOSTCMS') || exit('HostCMS: access denied.');
 class Core_Dump
 {
 	/**
+	 * 
+	 */
+	static public $depth = 3;
+
+	/**
+	 * 
+	 */
+	static public $protected = TRUE;
+
+	/**
+	 * 
+	 */
+	static public $private = TRUE;
+
+	/**
+	 * 
+	 */
+	static public $stringLength = 128;
+
+	/**
+	 * 
+	 */
+	static public $arrayWidth = 5;
+
+	/**
 	 * Обертка для вывода содержимого переменных.
 	 *
 	 * @return void
@@ -19,7 +44,7 @@ class Core_Dump
 			return;
 		}
 
-		self::_dump(func_get_args());
+		self::_dumpVars(func_get_args());
 	}
 
 	/**
@@ -34,7 +59,7 @@ class Core_Dump
 			return;
 		}
 
-		self::_dump(func_get_args());
+		self::_dumpVars(func_get_args());
 		exit;
 	}
 
@@ -44,15 +69,142 @@ class Core_Dump
 	 * @param  array  $aVariables
 	 * @return void
 	 */
-	static protected function _dump($aVariables)
+	static protected function _dumpVars($aVariables)
 	{
+		$aObjects = array();
+
 		print '<pre>';
 
 		foreach ($aVariables as $variable)
 		{
-			var_dump($variable);
+			print self::_getDumpVar($variable, 0, $aObjects);
 		}
 
 		print '</pre>';
+	}
+
+	/**
+	 *
+	 * @see https://www.leaseweb.com/labs/2013/10/smart-alternative-phps-var_dump-function/
+	 */
+	static protected function _getDumpVar($variable, $level = 0, &$aObjects = array())
+	{
+		$output = '';
+
+		switch (gettype($variable))
+		{
+			case 'boolean':
+				$output .= $variable ? 'true' : 'false';
+			break;
+
+			case 'integer':
+			case 'double':
+				$output .= $variable;
+			break;
+
+			case 'resource':
+				$output .= '[resource]';
+			break;
+
+			case 'NULL':
+				$output .= '';
+			break;
+
+			case 'unknown type':
+				$output .= '???';
+			break;
+
+			case 'string':
+				$length = strlen($variable);
+
+				// TODO: замена непечатаемых символов на печатаемые
+
+				if ($length > self::$stringLength)
+				{
+					$string = substr($variable, 0, self::$stringLength);
+					$string = '"' . $string . '"...';
+				}
+				else
+				{
+					$string = '"' . $variable . '"';
+				}
+
+				$output .= 'string(' . $length . '): ' . $string;
+			break;
+
+			case 'array':
+				$length = count($variable);
+
+				if (!$length)
+				{
+					$array = '';
+				}
+				elseif ($level == self::$depth)
+				{
+					$array  = '...';
+				}
+				else
+				{
+					$array = '';
+					$tabs = str_repeat(' ', $level * 2);
+					$countKeys = 0;
+
+					foreach ($variable as $key => $value)
+					{
+						if ($countKeys == self::$arrayWidth)
+						{
+							$array .= "\n" . $tabs . '  ...';
+							break;
+						}
+
+						$array .=  "\n" . $tabs . '  [' . $key . '] => ';
+						$array .=  self::_getDumpVar($value, $level + 1, $aObjects);
+						$countKeys++;
+					}
+
+					$array .= "\n" . $tabs;
+				}
+
+				$output .= 'array(' . $length . ') {' . $array . '}';
+			break;
+
+			case 'object':
+				$objectId = array_search($variable, $aObjects);
+
+				if ($objectId !== FALSE)
+				{
+					$output .= get_class($variable) . '#' . ($objectId + 1).' {...}';
+				}
+				elseif ($level == self::$depth)
+				{
+					$output .= get_class($variable).' {...}';
+				}
+				else
+				{
+					$objectId = array_push($aObjects, $variable);
+
+					$array = (array) $variable;
+					$tabs = str_repeat(' ', $level * 2);
+
+					$output .= get_class($variable) . "#$objectId\n" . $tabs . '{';
+					$aProperties = array_keys($array);
+
+					foreach ($aProperties as $property)
+					{
+						$output .= "\n" . $tabs . "  [$property] => ";
+						$output .= self::_getDumpVar($array[$property], $level + 1, $aObjects);
+					}
+
+					$output .= "\n" . $tabs . '}';
+				}
+			break;
+		}
+
+		if ($level == 0)
+		{
+			$output .= "\n";
+		}
+
+		return $output;
 	}
 }
